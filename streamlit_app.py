7ed3aa350ecf4e01e768b0fe2e3c99774cda3999
Chaitu8902 <chaitu8902@gmail.com>
2025-01-25 19:14:50 +0530

    Update streamlit_app.py
    made changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit as st
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
-st.title("🥤 Customize Your Smoothie! 🥤")
+st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
 st.write(
     """Choose the fruits you want in your custom smoothie!
     """
@@ -15,7 +15,7 @@ session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
-ingredients_list=st.multiselect('Choose upto 5 ingredients:', my_dataframe, max_selections==5)
+ingredients_list=st.multiselect('Choose upto 5 ingredients:', my_dataframe, max_selections=5)
 if ingredients_list:
     ingredients_string=''
     for fruit_chosen in ingredients_list:
